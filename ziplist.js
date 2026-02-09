59724dcd0b18774c5057ba04f21a2dd9145bb294
Jayden Cruz <jaydencruz1121@gmail.com>
2026-02-09 10:54:57 -1000

    Updated Index.html and ziplist

--- ziplist.js
+++ ziplist.js
@@ -1,1 +1,15 @@
 "use strict";
+const list1 = [1, 2, 3];
+const list2 = ['a', 'b', 'c'];
+function ziplist(arr1, arr2) {
+    const result = [];
+    for (let i = 0; i < arr1.length && i < arr2.length; i++) {
+        result.push([arr1[i], arr2[i]]);
+    }
+    return result;
+}
+console.log(ziplist(list1, list2));
+function ziplistTheFunctionalWay(arr1 = [1, 2, 3], arr2 = ['a', 'b', 'c']) {
+    return arr1.map((num, index) => [num, arr2[index]]).filter(pair => pair[1] !== undefined);
+}
+console.log(ziplistTheFunctionalWay(list1, list2));
--- ziplist.ts
+++ ziplist.ts
@@ -1,8 +1,8 @@
 const list1: number[] = [1, 2, 3];
 const list2: string[] = ['a', 'b', 'c'];
 
-function ziplist(arr1: number[], arr2: string[]) {
-  const result: [number, string][] = [];
+function ziplist(arr1: unknown[], arr2: unknown[]) {
+  const result: [unknown, unknown][] = [];
   for (let i = 0; i < arr1.length && i < arr2.length; i++) {
     result.push([arr1[i], arr2[i]]);
   }
@@ -11,7 +11,7 @@ function ziplist(arr1: number[], arr2: string[]) {
 
 console.log(ziplist(list1, list2));
 
-function ziplistTheFunctionalWay(arr1: number[] = [1, 2, 3], arr2: string[] = ['a', 'b', 'c']) {
+function ziplistTheFunctionalWay(arr1: unknown[], arr2: unknown[]) {
   return arr1.map((num, index) => [num, arr2[index]]).filter(pair => pair[1] !== undefined);
 }
 
